d420097f8f596593f7f5a51432f893c3ecc5f60d
rahma oshiba <rahma.oshiba@codezone-eg.com>
2024-11-11 16:36:22 +0200

    Validation and UI Done

--- Dtos/ApplicantDto.cs
+++ Dtos/ApplicantDto.cs
@@ -1,4 +1,5 @@
 ﻿using Recruitment.Attributes;
+using Recruitment.Localization;
 using System.ComponentModel.DataAnnotations;
 
 namespace Recruitment.Dtos
@@ -9,77 +10,75 @@ namespace Recruitment.Dtos
         public bool IsDoctor { get; set; }
         public decimal? ErpDepartmentPositionID { get; set; }
 
-        [Spaces(1, ErrorMessage = "Many Spaces")]
-        [StringLength(20, ErrorMessage = "The name exceeds the maximum allowed length.")]
-        [Required(ErrorMessage = "First Name is Required")]
-        [RegularExpression(@"^[\p{L} ]+$", ErrorMessage = "The input can only contain letters")]
+        [LocalizedCustomValidation.Spaces(1, "SpacesError")]
+        [LocalizedCustomValidation.LocalizedStringLength(20, "MaxLegnthError")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
+        [LocalizedCustomValidation.LocalizedRegularExpression(@"^[\p{L} ]+$", "LettersError")]
         public string FirstName { get; set; }
 
-        [Spaces(1, ErrorMessage = "Many Spaces")]
-        [StringLength(20, ErrorMessage = "The name exceeds the maximum allowed length.")]
-        [RegularExpression(@"^[\p{L} ]+$", ErrorMessage = "The input can only contain letters")]
-        [Required(ErrorMessage = "Second Name is Required")]
+        [LocalizedCustomValidation.Spaces(1, "SpacesError")]
+        [LocalizedCustomValidation.LocalizedStringLength(20, "MaxLegnthError")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
+        [LocalizedCustomValidation.LocalizedRegularExpression(@"^[\p{L} ]+$", "LettersError")]
         public string SecondName { get; set; }
 
-        [Spaces(1, ErrorMessage = "Many Spaces")]
-        [StringLength(20, ErrorMessage = "The name exceeds the maximum allowed length.")]
-        [Required(ErrorMessage = "Third Name is Required")]
-        [RegularExpression(@"^[\p{L} ]+$", ErrorMessage = "The input can only contain letters")]
+        [LocalizedCustomValidation.Spaces(1, "SpacesError")]
+        [LocalizedCustomValidation.LocalizedStringLength(20, "MaxLegnthError")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
+        [LocalizedCustomValidation.LocalizedRegularExpression(@"^[\p{L} ]+$", "LettersError")]
         public string ThirdName { get; set; }
 
-        [Spaces(1, ErrorMessage = "Many Spaces")]
-        [StringLength(20, ErrorMessage = "The Title exceeds the maximum allowed length.")]
+        [LocalizedCustomValidation.Spaces(1, "SpacesError")]
+        [LocalizedCustomValidation.LocalizedStringLength(20, "MaxLegnthError")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "Gender is Required")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public decimal? GenderID { get; set; }
 
-        [NotAllowFutureDate(ErrorMessage = "Future date is not allowed")]
-        [MinimumAge(15, ErrorMessage = "You must be older than 15 years old to apply.")]
-        [Required(ErrorMessage = "Birth date is Required")]
+        [LocalizedCustomValidation.NotAllowFutureDate("FutureError")]
+        [LocalizedCustomValidation.MinimumAge(15, "AgeError")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public DateTime? BirthDate { get; set; }
 
-        [StringLength(20, ErrorMessage = "The Phone exceeds the maximum allowed length.")]
-        //[RegularExpression(@"^[0-9]+$", ErrorMessage = "NumbersOnly")]
-        [RegularExpression(@"^\d{11,}$", ErrorMessage = "Phone number must contain only numbers and be at least 11 digits long.")]
-        [Required(ErrorMessage = "Phonen number is Required")]
+        [LocalizedCustomValidation.LocalizedStringLength(20, "MaxLegnthError")]
+        [LocalizedCustomValidation.LocalizedRegularExpression(@"^\d{11,}$", "PhoneNumberError")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public string PhoneNumber { get; set; }
 
-        [StringLength(200, ErrorMessage = "The Address exceeds the maximum allowed length.")]
-        [Required(ErrorMessage = "Address is Required")]
+        [LocalizedCustomValidation.LocalizedStringLength(200, "MaxLegnthError")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public string Address { get; set; }
 
-        [Required(ErrorMessage = "Area is Required")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public decimal? ErpAreaCityID { get; set; }
 
-        [StringLength(254, ErrorMessage = "The email address exceeds the maximum allowed length of 254 characters.")]
-        [Required(ErrorMessage = "Email is Required")]
-        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Please enter a valid email address.")]
-        //[EmailAddress] 
+        [LocalizedCustomValidation.LocalizedStringLength(254, "MaxLegnthError")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
+        [LocalizedCustomValidation.LocalizedRegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", "EmailError")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Martial status is Required")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public decimal? ErpMartialStatusID { get; set; }
 
-        [Required(ErrorMessage = "Militry status is Required")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public decimal? ErpMilitryStatusID { get; set; }
 
         public string CV { get; set; }
 
-        [Required(ErrorMessage = "GraduationYear is Required")]
-        [CustomYearRange(1980, ErrorMessage = "Graduation Year must be between 1980 and the current year.")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
+        [LocalizedCustomValidation.CustomYearRange(1980, "GraduationYearError")]
         public int? GraduationYear { get; set; }
 
-        [Required(ErrorMessage = "University is Required")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public decimal? ErpUniversityID { get; set; }
 
-        [Required(ErrorMessage = "Doctor degree is Required")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public decimal? ErpDoctorDegreeID { get; set; }
 
-        [Required(ErrorMessage = "Branch is Required")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public decimal? ErpBranchID { get; set; }
 
-        [Required(ErrorMessage = "Specialty is Required")]
+        [LocalizedCustomValidation.LocalizedRequired("Required")]
         public decimal? ErpSpecialtyID { get; set; }
 
         public decimal? ErpOtherSpecialtyID { get; set; }
